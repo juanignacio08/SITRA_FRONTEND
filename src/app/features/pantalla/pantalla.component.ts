ba6aa7071147b470c828439c2af846a81c1e52bb
creifer <creifer>
2025-11-04 21:03:55 -0500

    pantalla

--- src/app/features/pantalla/pantalla.component.ts
+++ src/app/features/pantalla/pantalla.component.ts
@@ -1,31 +1,56 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+interface Paciente {
+  codigo: string;
+  nombre: string;
+  estado:string;
+}
 
 @Component({
   selector: 'app-pantalla',
   imports: [
-    MatIconModule,
-    MatListModule,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './pantalla.component.html',
-  styleUrl: './pantalla.component.css'
+  styleUrls: ['./pantalla.component.css']
 })
-export class PantallaComponent {
-  pacienteVentanilla1: string = 'Juan Pérez';
-  pacienteVentanilla2: string = 'María Gómez';
+export class PantallaComponent implements OnInit, OnDestroy {
 
-  listaPacientes: string[] = [
-    'Pedro Martínez',
-    'Ana Torres',
-    'Luis Fernández',
-    'Carla Rojas',
-    'David López',
-    'Sofía Ramírez'
-  ];
+  // Pacientes actualmente en ventanillas
+  ventanilla1: Paciente = { codigo: 'A001', nombre: 'Pedro Martínez',estado:"atendiendo" };
+  ventanilla2: Paciente = { codigo: 'A002', nombre: 'Ana Torres',estado:"atendiendo" };
+
+  // Próximos pacientes
+  proximosPacientes = [
+  { codigo: 'A003', nombre: 'Carlos Alberto Francisco Fernández Pérez', estado:"pendiente" },
+  { codigo: 'A004', nombre: 'David Alejandro Manuel López Torres',estado:"pendiente" },
+  { codigo: 'A005', nombre: 'Elena Sofía Isabel Castro Díaz',estado:"pendiente" },
+  { codigo: 'A006', nombre: 'Laura Victoria Isabel Giménez Morales',estado:"pendiente" },
+  { codigo: 'A007', nombre: 'Carla Fernanda Beatriz Rojas Sánchez',estado:"pendiente" },
+  { codigo: 'A008', nombre: 'Sofía Elena Margarita Ramírez Flores',estado:"pendiente" },
+  { codigo: 'A003', nombre: 'Carlos Alberto Francisco Fernández Pérez' ,estado:"pendiente"},
+  { codigo: 'A004', nombre: 'David Alejandro Manuel López Torres',estado:"pendiente"},
+  { codigo: 'A005', nombre: 'Elena Sofía Isabel Castro Díaz',estado:"pendiente" },
+  { codigo: 'A006', nombre: 'Laura Victoria Isabel Giménez Morales',estado:"pendiente" },
+  { codigo: 'A007', nombre: 'Carla Fernanda Beatriz Rojas Sánchez',estado:"pendiente" },
+  { codigo: 'A008', nombre: 'Sofía Elena Margarita Ramírez Flores',estado:"pendiente" },
+];
+
+
+  horaActual: Date = new Date();
+  private timer: any;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => this.horaActual = new Date(), 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
+
+  horaFormateada(): string {
+    return this.horaActual.toLocaleTimeString('es-PE', { hour12: false });
+  }
 
 }
-
-
