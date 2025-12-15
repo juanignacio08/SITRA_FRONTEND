e433a844b5767cb4a06fe9e3eab5a1b283da759d
creifer <creifer>
2025-12-14 23:42:00 -0500

    mejora de contenedores de admin, rec y ase

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.18.32:8080/sitra/api/v1', // URL del backend en desarrollo
+  apiUrl: 'http://localhost:8080/sitra/api/v1', // URL del backend en desarrollo
 };
 
 
